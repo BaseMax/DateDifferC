48418418dbdfbaee4949ea83c320e63eecb10f93
Max Base <MaxBaseCode@Gmail.Com>
2022-06-10 17:35:15 +0430

    Create date-diff.c

---
+++ date-diff.c
@@ -0,0 +1,14 @@
+/*
+ * Date: 2022/06/10
+ * Author: Max Base
+ * Name: Date Differ (C)
+ * Repository: github.com/basemax/DateDifferC
+ * Description: Calculates the difference between two dates with old format.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+char *strptime(const char * __restrict, const char * __restًrict, struct tm * __restrict);
