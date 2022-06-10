3f7e0b4b9a57e3d3206938562b7bec2ebbb3985a
Max Base <MaxBaseCode@Gmail.Com>
2022-06-10 17:52:42 +0430

    Update date-diff.c

--- date-diff.c
+++ date-diff.c
@@ -172,15 +172,3 @@ char* date_diff(const char* date_start, const char* date_end) {
     // return ago;
 }
 
-int main() {
-    const char* date_start = "1970-01-01 00:00:01";
-    const char* date_end = "2038-01-19 03:14:07";
-    
-    char* diff_ago = date_diff(date_start, date_end);
-    printf("%s\n", diff_ago);
-
-    diff_ago = date_diff(date_end, date_start);
-    printf("%s\n", diff_ago);
-
-    return 0;
-}
